17d94a6eff61c3693b023152ae337a4e835d75e4
Alexander Zvaniga <alex.zvaniga@gmail.com>
2018-08-16 19:41:36 +0000

    added user model, routes and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     resources :categories, except: [:show]
   end
 
+  resource  :user, only: [:new, :create] 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
