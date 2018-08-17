8aa723cbaf85ed6450ea9add790a8e8370325461
Your Name <alex.zvaniga@gmail.com>
2018-08-16 23:34:42 -0400

    updated routes for logout

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/login' => 'sessions#new'
+  get '/logout' => 'sessions#destroy'
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
