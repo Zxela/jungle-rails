a535e0ebd96f447e000932717fa0e8b69fd27e14
Your Name <alex.zvaniga@gmail.com>
2018-08-16 23:35:17 -0400

    route udpates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login', notice: 'You have been logged out'
+    redirect_to '/', notice: 'You have been logged out'
   end
 
 end
