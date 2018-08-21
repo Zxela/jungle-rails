36e08fa001da8e560ffdbe34e30696a503c76713
Your Name <alex.zvaniga@gmail.com>
2018-08-21 12:11:15 -0400

    feature

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,4 @@
 class Admin::ApplicationController < ApplicationController
 
-  http_basic_authenticate_with name: ENV['ADMIN_ACC'], password: ENV['ADMIN_PASS']
 
 end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
-class Admin::DashboardController < Admin::ApplicationController
+class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_ACC'], password: ENV['ADMIN_PASS']
 
   def show
   end
